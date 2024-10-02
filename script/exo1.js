7635df0d396344ea8a68c07173d19531f8210ec4
ElmahdiK <elmahdi.korfed@gmail.com>
2024-10-02 11:07:21 +0200

    fix(exo1): ajout d'une boucle pour obtenir la somme

--- script/exo1.js
+++ script/exo1.js
@@ -1,39 +1,47 @@
 /*
-
+--- --- ---
 Exercice  1 :
 Vous organisez un concours de dev où chaque participant a trois épreuves. Selon la moyenne des
 scores, attribuez les niveaux suivants :
 Moins de 50 : "Débutant"
 Entre 50 et 70 : "Intermédiaire"
 Plus de 70 : "Expert"
-
-
+--- --- ---
 Début 
-
 Lire les 3 notes d'épreuves de chaque participant
-Pour participant, calculer sa moyenne
+Pour chaque participant, calculer sa moyenne
 Si la moyenne est < à 50 afficher : "Débutant"
 Si la moyenne est entre 50 et 70 afficher : "Débutant"
 Si la moyenne est > à 70 afficher : "Expert"
 Fin
+--- --- ---
 */
 
 window.onload = _ => {
     console.log("page loaded");
 
-    let participants = [[40,10,25],[85,90,100],[45,60,70]];
+    // résultats des épreuves
+    const RESULTATS = [[10, 20, 45], [50, 65, 80], [90, 95, 100]];
     let moyenne = 0;
+    let somme = 0;
 
-    for (let participant in participants) {
-        // calcul de la moyenne
-        moyenne = ((participants[participant][0] + participants[participant][1] + participants[participant][2])/3)
-        console.log(`Le participant ${participant} a une moyenne de : ${moyenne} => ${niveau(moyenne)}`)
+    // pour chaque participant
+    for (let participant in RESULTATS) {
+        somme = 0;
+        // on calcul la somme des résultats obtenus
+        for (let note in RESULTATS[participant]) {
+            somme += RESULTATS[participant][note];
+        }
+        // on calcul la moyenne
+        moyenne = somme / RESULTATS[participant].length;
+        console.log(`Le participant ${participant} a une moyenne de : ${moyenne} => ${getNiveau(moyenne)}`)
     }
 }
 
-const niveau = (moyenne) => {
-    if (moyenne < 50) return "Débutant";
-    else if (moyenne >= 50 && moyenne <= 70) return "Intermédiaire";
-    else return "Expert";
+const NIVEAUX = ["Débutant", "Intermédiaire", "Expert"];
+const getNiveau = (moyenne) => {
+    if (moyenne < 50) return NIVEAUX[0];
+    else if (moyenne >= 50 && moyenne <= 70) return NIVEAUX[1];
+    else return NIVEAUX[2];
 }
 
